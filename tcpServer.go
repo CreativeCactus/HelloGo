deeea8a1024799015a29037d8f19b23bc460ffd5
CreativeCactus <matt3dixon@gmail.com>
2016-05-05 10:13:14 +0000

    Cleaned up a bit Removed pasta Added test boilerplate

--- tcpServer.go
+++ tcpServer.go
@@ -10,8 +10,6 @@ import (
   "reflect"
   "strconv"
   
-	"github.com/HouzuoGuo/tiedot/db" //Replace with file io
-  //"github.com/HouzuoGuo/tiedot/dberr"
   /*
   "io/ioutil"
   "os"
@@ -25,30 +23,20 @@ func main() {
   // Config
   proto := "tcp"
   port  := ":8081"
-  dbPath:= "./godb"
   
   // Storage
-	DB, err := db.OpenDB(dbPath)
-	cols:=DB.AllCols()
-	print("Loaded DB with cols: %v",cols);
-	print("DBG:%v",in(cols,"Users"))
-	if len(cols)==0 {
-	  if err := DB.Create("Users"); err != nil {		panic(err)	}
-	  print("INIT:Created Users collection.");
-	}
-	
-	if err != nil {		panic(err)	}
+  //......
   
   // Blocking Server
   print("Waiting for connections on %v %v",proto,port)
   session, _ := net.Listen(proto,port)
   for {
     conn, _ := session.Accept()
-    go fmt.Println(handleComms(conn,DB))
+    go fmt.Println(handleComms(conn))
   }
 }
 
-func handleComms(conn  net.Conn, DB *db.DB) string{
+func handleComms(conn  net.Conn) string{
   fmt.Printf("User connected from %v\n",conn.RemoteAddr());
   conn.Write([]byte("Hi. Do you have ID?\n"))
   ID, _ := bufio.NewReader(conn).ReadString('\n')
@@ -57,14 +45,10 @@ func handleComms(conn  net.Conn, DB *db.DB) string{
   print("DBG:%v",UID)
   
   //if err := DB.Create("Feeds"); err != nil {		panic(err)	}
-  DBusers := DB.Use("Users")
-  if value,err := DBusers.Read(UID); err!=nil||value==nil {
-    //conn.Close(); 
-    //return fmt.Sprintf("Declined new user: %v",value);
-  } else {
-    fmt.Sprintf("Accepted user: %v",value); 
-    
-  }
+  //conn.Close(); 
+  //return fmt.Sprintf("Declined new user: %v",value);
+  fmt.Sprintf("Accepted user: %v",UID); 
+  
   //fmt.Printf("%v\n", value)
   //db.Erase(key)
   
@@ -112,41 +96,6 @@ func instr(haystack []string, needle string) int{
     return haystack[i]==needle;
   })
 }
-
-func anySlice(arg interface{}) (out []interface{}) {
-  slice, success := func (arg interface{}, kind reflect.Kind) (val reflect.Value, ok bool) {
-      val = reflect.ValueOf(arg)
-      ok = (val.Kind() == kind)
-      return
-    }(arg, reflect.Slice)
-  if !success {        return nil  }
-  c := slice.Len()
-  out = make([]interface{}, c)
-  for i := 0; i < c; i++ {
-      out[i] = slice.Index(i).Interface()
-  }
-  return out
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//func ĳ(){}//T* to interface
-
-
-
-
-
 
 
 
---
+++ tcpServer_test.go
@@ -0,0 +1,8 @@
+package main 
+import "testing" 
+    
+func TestTimeConsuming(t *testing.T) {
+    if testing.Short() {
+        t.Skip("skipping test in short mode.")
+    }
+}
